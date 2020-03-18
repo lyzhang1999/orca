b634042947dc0f9ae322f5d80e0e7bc9927dfc33
kun <451036329@qq.com>
2020-03-18 13:46:43 +0800

    fix script docker login

--- build.sh
+++ build.sh
@@ -10,14 +10,15 @@ ARTIFACT=orca
 
 COMMIT_ID=$(git rev-parse HEAD)
 
+if ! grep -q ${CODING_REGISTRY_URL} ~/.docker/config.json; then
+    echo "logging into ${CODING_REGISTRY_URL}..."
+    echo ${CODING_REGISTRY_PASS} | docker login -u ${CODING_REGISTRY_USER} --password-stdin ${CODING_REGISTRY_URL} || echo "[ERROR] Login $CODING_REGISTRY_URL failed"
+fi
+
 docker build -t ${ARTIFACT}-compile -f Dockerfile.compile .
 docker build -t ${ARTIFACT}:latest -f Dockerfile.slim .
 
 if [ "$1" = "--push" ]; then
-  if ! grep -q ${CODING_REGISTRY_URL} ~/.docker/config.json; then
-    echo "logging into ${CODING_REGISTRY_URL}..."
-    echo ${CODING_REGISTRY_PASS} | docker login -u ${CODING_REGISTRY_USER} --password-stdin ${CODING_REGISTRY_URL} || echo "[ERROR] Login $CODING_REGISTRY_URL failed"
-  fi
   docker tag ${ARTIFACT}:latest ${CODING_REGISTRY_URL}/${CODING_REGISTRY_REPO}/${ARTIFACT}:${COMMIT_ID}
   docker push ${CODING_REGISTRY_URL}/${CODING_REGISTRY_REPO}/${ARTIFACT}:${COMMIT_ID}
 fi
