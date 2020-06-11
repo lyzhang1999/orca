33b354857288f7d0a72102d79283f60da7b393c1
aaron <8206503@qq.com>
2020-06-11 14:09:24 +0800

    替换 YAML_CONTENT

--- orca-clouddriver/src/main/groovy/com/netflix/spinnaker/orca/clouddriver/tasks/providers/kubernetes/KubernetesJobRunner.java
+++ orca-clouddriver/src/main/groovy/com/netflix/spinnaker/orca/clouddriver/tasks/providers/kubernetes/KubernetesJobRunner.java
@@ -16,20 +16,46 @@
 
 package com.netflix.spinnaker.orca.clouddriver.tasks.providers.kubernetes;
 
+import static java.util.Collections.emptyList;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.netflix.spinnaker.kork.artifacts.model.Artifact;
+import com.netflix.spinnaker.kork.core.RetrySupport;
+import com.netflix.spinnaker.orca.clouddriver.OortService;
 import com.netflix.spinnaker.orca.clouddriver.tasks.job.JobRunner;
 import com.netflix.spinnaker.orca.clouddriver.tasks.manifest.ManifestContext;
 import com.netflix.spinnaker.orca.clouddriver.tasks.manifest.ManifestEvaluator;
 import com.netflix.spinnaker.orca.clouddriver.tasks.manifest.RunJobManifestContext;
 import com.netflix.spinnaker.orca.pipeline.model.Stage;
 import com.netflix.spinnaker.orca.pipeline.util.ArtifactResolver;
-import java.util.*;
+import io.micrometer.core.instrument.util.IOUtils;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.yaml.snakeyaml.Yaml;
+import org.yaml.snakeyaml.constructor.SafeConstructor;
+import retrofit.client.Response;
 
 @Component
 @Data
+@Slf4j
 public class KubernetesJobRunner implements JobRunner {
+  private static final ThreadLocal<Yaml> yamlParser =
+      ThreadLocal.withInitial(() -> new Yaml(new SafeConstructor()));
+
+  private static final String PREFIX_CUSTOM_ENV = "YAML_CONTENT";
+
+  @Autowired OortService oort;
+
+  private final RetrySupport retrySupport = new RetrySupport();
 
   private boolean katoResultExpected = false;
   private String cloudProvider = "kubernetes";
@@ -71,11 +97,87 @@ public class KubernetesJobRunner implements JobRunner {
       operation.put("optionalArtifacts", result.getOptionalArtifacts());
     }
 
+    try {
+      replaceEnvArtifacts(stage);
+    } catch (Exception e) {
+      log.warn("replaceEnvArtifacts fail", e);
+    }
+
     KubernetesContainerFinder.populateFromStage(operation, stage, artifactResolver);
 
     Map<String, Object> task = new HashMap<>();
     task.put(OPERATION, operation);
     return Collections.singletonList(task);
+  }
+
+  @SuppressWarnings("unchecked")
+  private void replaceEnvArtifacts(Stage stage) {
+    RunJobManifestContext runJobManifestContext = stage.mapTo(RunJobManifestContext.class);
+
+    List<Map<String, Object>> containers =
+        Optional.of(stage.getContext())
+            .map(o -> (Map<String, Object>) o.get("manifest"))
+            .map(o -> (Map<String, Object>) o.get("spec"))
+            .map(o -> (Map<String, Object>) o.get("template"))
+            .map(o -> (Map<String, Object>) o.get("spec"))
+            .map(o -> (List<Map<String, Object>>) o.get("containers"))
+            .orElse(emptyList());
+
+    List<Artifact> requiredArtifacts = new ArrayList<>();
+    List<String> artifactIds =
+        Optional.ofNullable(runJobManifestContext.getRequiredArtifactIds()).orElse(emptyList());
+    for (String id : artifactIds) {
+      Artifact requiredArtifact = artifactResolver.getBoundArtifactForId(stage, id);
+      if (requiredArtifact == null) {
+        throw new IllegalStateException(
+            "No artifact with id '" + id + "' could be found in the pipeline context.");
+      }
+
+      requiredArtifacts.add(requiredArtifact);
+    }
+
+    for (Map<String, Object> container : containers) {
+      List<Map<String, Object>> envs =
+          (List<Map<String, Object>>) Optional.ofNullable(container.get("env")).orElse(emptyList());
+      for (Map<String, Object> env : envs) {
+        String key = (String) env.get("name");
+        if (key != null && key.startsWith(PREFIX_CUSTOM_ENV)) {
+          injectContent(env, requiredArtifacts);
+        }
+      }
+    }
+  }
+
+  private void injectContent(Map<String, Object> envItem, List<Artifact> requiredArtifacts) {
+    Artifact manifestArtifact = objectMapper.convertValue(envItem.get("value"), Artifact.class);
+    String rawManifests =
+        retrySupport.retry(
+            () -> {
+              try {
+                Response manifestText = oort.fetchArtifact(manifestArtifact);
+                if (manifestText.getStatus() == 200) {
+                  return IOUtils.toString(manifestText.getBody().in());
+                }
+                throw new IllegalStateException(
+                    String.format("fetchArtifact error: %s", manifestText.getStatus()));
+              } catch (Exception e) {
+                throw new IllegalStateException(e);
+              }
+            },
+            10,
+            Duration.ofMillis(200),
+            true); // retry 10x, starting at .2s intervals);
+    log.debug("YAML_CONTENT raw\n{}", rawManifests);
+
+    if (rawManifests != null) {
+      for (Artifact artifact : requiredArtifacts) {
+        rawManifests = rawManifests.replace(artifact.getName(), artifact.getReference());
+      }
+      log.debug("YAML_CONTENT raw replace artifact\n{}", rawManifests);
+      envItem.put("value", rawManifests);
+    } else {
+      log.error("YAML_CONTENT raw is empty");
+    }
   }
 
   public Map<String, Object> getAdditionalOutputs(Stage stage, List<Map> operations) {
